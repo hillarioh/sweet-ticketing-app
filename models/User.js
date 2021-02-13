3800b5d5fddc83aed80a4bf3b3c8778e2d7bd8df
hillarioh <okerioh@gmail.com>
2021-02-13 12:33:28 +0300

    add bcrypt and callback function

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -27,7 +28,12 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.pre("save", async function () {
   if (!this.isModified("password")) {
+    next();
   }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("user", UserSchema);
