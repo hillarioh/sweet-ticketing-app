eeb6bae93bf8e9b57902b1d130ede0c711f7ad64
hillarioh <okerioh@gmail.com>
2021-02-13 12:22:33 +0300

    add User model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "please provide a username"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      ,
+      "Please provide a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+  }
+});
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
