0bc79165421b43615b31660dc97d9f463ca5cb26
hillarioh <okerioh@gmail.com>
2021-02-13 13:57:27 +0300

    add database credential and error handling

--- models/User.js
+++ models/User.js
@@ -26,7 +26,7 @@ const UserSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 });
 
-UserSchema.pre("save", async function () {
+UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
   }
