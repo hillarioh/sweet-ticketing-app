971bc7079b2e12f5da4a3dbe26485790f76586d4
hillarioh <okerioh@gmail.com>
2021-02-13 11:23:43 +0300

    add dotenv, env and edit server env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+require("dotenv").config({ path: "./config/env" });
+const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=> console.log(`Sever running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Sever running on port ${PORT}`));
