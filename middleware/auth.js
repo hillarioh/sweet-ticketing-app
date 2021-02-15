a2de9e881976a699904b48d357ff05f974da0709
hillarioh <okerioh@gmail.com>
2021-02-15 17:49:54 +0300

    add private routes and authentication logic

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ exports.protect = async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
-    req.headers.authorization.startswith("Bearer")
+    req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
   }
